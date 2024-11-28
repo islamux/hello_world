b6e8b41b930061987bdb205df7f5ea725b34d03b
islamux <fathi733@gmail.com>
2024-11-28 07:57:39 +0300

    refactor : 1- rename the app 2- move to seperate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,8 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hello_world/home_page.dart';
-import 'package:hello_world/language_cubit.dart';
-import 'package:hello_world/helpers/localization_delegates.dart';
-import 'package:hello_world/static/supported_locales.dart';
+import 'package:hello_world/hello_world.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LanguageCubit(),
-      child: BlocBuilder<LanguageCubit, Locale>(
-        builder: (context, locale) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Localized App',
-            localizationsDelegates: localizationDelegates,
-            supportedLocales: supportedLocales,
-            locale: locale,
-            theme: ThemeData(primarySwatch: Colors.blue),
-            home: const HomePage(),
-          );
-        },
-      ),
-    );
-  }
+  runApp(const HelloWorld());
 }
